7205f1d93dadbe4ada7049a21789574cc9852f7a
Pavle Stojanovic <Pavle.Stojanovic@rt-rk.com>
2022-09-13 12:08:35 +0200

    Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+  const handleClick = (e) => {
+    console.log('hello ninjas', e);
+  }
+
+  const handleClickAgain = (name, e) => {
+    console.log('Hello ' + name, e.target);
+  }
+
   return ( 
     <div className='home'>
       <h1>Homepage</h1>
+      <button onClick={handleClick} >Click me</button>
+      <button onClick={(e) => handleClickAgain('Pavle', e)}>Click me again</button>
     </div>
    );
 }
