0dd26c84600773f10f2c137e7ec2aafa32c482e7
Pavle Stojanovic <Pavle.Stojanovic@rt-rk.com>
2022-09-13 10:55:39 +0200

    Diynamic Values in Templates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
 
 function App() {
+
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  // const person = {name: 'Pavle', age: 32}
+  const link = 'https://www.google.com'
+
   return (
     <div className="App">
-      <h1>App component</h1>
+      <h1>{title}</h1>
+      <p>Likes: {likes} times</p>
+
+      {/* <p>{person}</p> */}
+
+      <p>{ 10 }</p>
+      <p>{'Hello ninjas'}</p>
+      <p>[1,2,3,4,5]</p>
+      <p>{Math.random() * 10}</p>
+
+      <a href={link}>Google site</a>
+
     </div>
   );
 }
